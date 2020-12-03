38d5ffd6794a12fbbde159b3ccb2c2de6201b506
ITGXZX-FSG003\weishiyao <inwsy@hotmail.com>
2020-12-03 17:31:41 +0800

    新增 「备忘录模式」 示例代码

---
+++ src/com/geekdigging/chapter17/memento/Caretaker.java
@@ -0,0 +1,17 @@
+package com.geekdigging.chapter17.memento;
+
+/**
+ * 备忘录管理员
+ */
+public class Caretaker {
+    // 备忘录对象
+    private Memento memento;
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
